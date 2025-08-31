2000432a4142c4a73983c91e5d7c501afb335c79
todmiv <todmiv@outlook.com>
2025-08-31 23:50:48 +0300

    Добавлена настройка для GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,29 @@
 // src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 
-// Получаем элемент контейнера
-const container = document.getElementById('root');
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    lazy: () => import('./pages/LoginPage'),
+  },
+  {
+    path: "/documents",
+    lazy: () => import('./pages/DocumentsPage'),
+  }
+], {
+  basename: '/sro-noso-webapp/'
+})
 
-// Проверяем, что контейнер существует
-if (container) {
-  // Создаем корневой элемент React
-  const root = ReactDOM.createRoot(container);
-  // Рендерим приложение
-  root.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>,
-  )
-} else {
-  // Обрабатываем случай, если элемент не найден (например, выводим ошибку в консоль)
-  console.error("Критическая ошибка: Элемент с ID 'root' не найден в DOM. Приложение не может быть запущено.");
-  // Или можно отобразить сообщение об ошибке непосредственно в документе:
-  // document.body.innerHTML = "<h1>Критическая ошибка загрузки приложения. Элемент 'root' не найден.</h1>";
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
